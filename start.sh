b685dbaeff77b70a9eaf482c84e61759bb2e293b
Arsen Nuramatov <anuramat@pm.me>
2022-07-16 00:36:11 +0300

    removed start script (using compose instead)

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# temporary container, completely deleted when exited
-docker run --rm -it \
---entrypoint bash \
---mount type=bind,source="$(pwd)",target=/code,readonly \
---env-file=.env \
-ks-tha
