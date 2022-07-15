2a45237050879253f8253ff1ffa5f0c3a6f44ff0
Arsen Nuramatov <anuramat@pm.me>
2022-07-15 22:14:49 +0300

    whoops

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def file_changed():
+    return "nothing here..."
+
+
+@app.route("/update", methods=["POST"])
+def result():
+    # X-Goog-Channel-Id: bc127763-1ed4-4796-b7ea-c5ce9574fa93
+    # X-Goog-Channel-Expiration: Fri, 15 Jul 2022 18:06:18 GMT
+    # X-Goog-Resource-State: update
+    # X-Goog-Changed: content
+    # X-Goog-Message-Number: 3236479
+    # X-Goog-Resource-Id: jv2sI78Oz0nntsXBXfHxhJYS0Ig
+    # X-Goog-Resource-Uri: https://www.googleapis.com/drive/v3/files/1oy13To3eyYlNDEs49TekBWFBKLycmoq0-wAjl2kEOdY?acknowledgeAbuse=false&alt=json&supportsAllDrives=false&supportsTeamDrives=false&alt=json
+    # User-Agent: APIs-Google; (+https://developers.google.com/webmasters/APIs-Google.html)
+    # Accept-Encoding: gzip, deflate, br
+    
+    # print(request.headers, flush=True)
+    print(request.headers.get('X-Goog-Resource-State'), flush=True)
+    return 'xd'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
